bd6b2020f93cb167f058ccdf40fb029b07e2207b
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2021-12-01 19:43:50 +0900

    ログイン機能完了

---
+++ Rails/config/application.rb
@@ -0,0 +1,32 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module App
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    config.generators.template_engine = :slim 
+
+    config.generators do |g|  
+      g.assets false          
+      g.skip_routes false     
+      g.test_framework false 
+    end               
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+  end
+end
