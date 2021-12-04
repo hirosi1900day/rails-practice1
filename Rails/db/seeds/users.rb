4d3576a34c9741e9efe8d0647e4e7ca97531eeb6
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2021-12-04 20:43:22 +0900

    complete

--- Rails/db/seeds/users.rb
+++ Rails/db/seeds/users.rb
@@ -1,7 +1,10 @@
+puts 'Start inserting seed "users" ...'
 20.times do |n|
     User.create!(
+        name: Faker::Name.name,
         email: Faker::Internet.unique.email,
         password: "password",
         password_confirmation: "password"        
-)
+    )
+    
 end
