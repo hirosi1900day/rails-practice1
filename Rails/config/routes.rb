3cb515d32e8d68ea01a1fc1d1d128513b39def22
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2021-12-06 02:20:43 +0900

    model, controller, veiwファイル作成

--- Rails/config/routes.rb
+++ Rails/config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'  
   delete 'logout', to: 'user_sessions#destroy'  
 
-  resources 'posts'
+  resources 'posts' do
+    resources :comments, shallow: true
+  end
 
 
   resources :users, only: %i[new create]
