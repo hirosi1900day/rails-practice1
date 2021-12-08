f4c1ef3b8ebbd333327ffac6e569e94f403fd3f3
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2021-12-08 12:51:25 +0900

    controller作成完了

--- Rails/app/models/post.rb
+++ Rails/app/models/post.rb
@@ -20,6 +20,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   mount_uploaders :images, PostImageUploader
   serialize :images, JSON
--- Rails/app/models/user.rb
+++ Rails/app/models/user.rb
@@ -18,6 +18,8 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :post
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
@@ -26,5 +28,17 @@ class User < ApplicationRecord
 
   def own?(object)
     id == object.user_id
+  end
+
+  def like(post)
+    like_posts.append(post)
+  end
+
+  def unlike(post)
+   like_posts.destroy(post)
+  end
+
+  def like?(post)
+    like_posts.include?(post)
   end
 end
